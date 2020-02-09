5818730f1bccdfed764820c72a139d09e5ede5a2
John <jjmarsha@usc.edu>
2020-02-09 04:17:48 -0800

    push to pord

--- portfolio/src/pages/page.js
+++ portfolio/src/pages/page.js
@@ -8,6 +8,17 @@ const Page = (props) => {
             <div className="page">
                 {props.children}
             </div>
+            <div className="footer">
+                <a href="https://www.linkedin.com/in/john-marshall-60113412a/">
+                    <i class="fab fa-linkedin"/>
+                </a>
+                <a href="https://github.com/jjmarsha">
+                    <i class="fab fa-github-square"></i>
+                </a>
+                <a href="mailto:jjmarsha@usc.edu">
+                    <i class="fas fa-envelope-square"></i>
+                </a>
+            </div>
         </Col>
     )
 }
