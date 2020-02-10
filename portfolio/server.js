c74d131cda2f61b8de6fc38e0b27f0fdacf405de
John <jjmarsha@usc.edu>
2020-02-09 19:15:37 -0800

    easy deploy

--- portfolio/server.js
+++ portfolio/server.js
@@ -33,5 +33,5 @@ app.get('/', (req, res) => {
 })
 
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0", () => console.log("Running Server at " + port));
